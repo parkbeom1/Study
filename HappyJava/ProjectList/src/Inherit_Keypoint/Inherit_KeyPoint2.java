e812ff24a255e10cca4e5154e82c5acc70a08e71
zkokopo <zkokopo@naver.com>
2020-08-23 18:11:08 +0900

    java File [ADD]

---
+++ HappyJava/ProjectList/src/Inherit_Keypoint/Inherit_KeyPoint2.java
@@ -0,0 +1,122 @@
+package Inherit_Keypoint;
+
+/*
+<요구사항>
+문제 풀어 보세요 ^^
+요구사항
+카트 (Cart)
+카트에는 매장에 있는 모든 전자제품을 담을 수 있다 
+카트의 크기는 고정되어 있다 (10개) : 1개  , 2개 담을 수 있고 최대 10개까지 담을 수 있다
+고객이 물건을 구매 하면 ... 카트에 담는다
+계산대에 가면 전체 계산
+계산기능이 필요
+summary() 기능 추가해 주세요
+당신이 구매한 물건이름 과 가격정보 나열
+총 누적금액 계산 출력
+hint) 카트 물건을 담는 행위 (Buy() 함수안에서 cart  담는 것을 구현 )  - 제품들이여러개 카트는 하나 
+hint) Buyer ..>> summary()  main 함수에서 계산할때
+구매자는 default 금액을 가지고 있고 초기금액을 설정할 수 도 있다
+*/
+class Product2 {
+	int price;
+	int bonuspoint;
+	
+	public Product2(int price) {
+		this.price = price;
+		this.bonuspoint = (int)(price / 10.0);
+	}
+}
+class Tv2 extends Product2 {
+	
+	Tv2() {
+		super(500);
+	}
+	public String toString() {
+		return "Tv ";
+	}
+}
+class Radio2 extends Product2 {
+	
+	Radio2() {
+		super(250);
+	}
+	public String toString() {
+		return "라디오";
+	}
+}
+class Audio2 extends Product2 {
+	
+	Audio2() {
+		super(350);
+	}
+	public String toString() {
+		return "오디오 ";
+	}
+}
+
+class Buyer2 {
+	int money;
+	int bonuspoint;
+	Product2 Cart[];
+	int index = 0;
+	
+	Buyer2(int money,int bonuspoint) {
+		this.money = money;
+		this.bonuspoint = bonuspoint;
+		Cart = new Product2[10];
+	}
+	
+	public void buy(Product2 product) {
+		
+		if(this.money < product.price) {
+			System.out.println("잔액이 부족합니다.");
+		}
+		if(this.index >= 10) {
+			System.out.println("더이상 담을수 없습니다.");
+		}
+		
+		Cart[index++] = product;
+		
+		this.money -= product.price;
+		this.bonuspoint += product.bonuspoint;
+		System.out.println("구입 물품은 : " + product.toString());
+		System.out.println("내 남은 금액은 " + this.money);
+		System.out.println("내 누적 포인트는 " + this.bonuspoint);
+	}
+	
+	public void summary() {
+		int totalprice = 0;
+		int totalbonuspoint = 0;
+		String totalList ="";
+		
+		for(int i = 0; i < index; i ++) {
+			totalprice += Cart[i].price;
+			totalbonuspoint += Cart[i].bonuspoint;
+			totalList += Cart[i].toString();
+		}
+		
+		System.out.println("총 구매 금액은 " + totalprice);
+		System.out.println("총 누적 포인트는 " + totalbonuspoint);
+		System.out.println("총 구매 목록은 " + totalList + " ");
+	}
+}
+public class Inherit_KeyPoint2 {
+	public static void main(String[] args) {
+		Buyer2 buyer = new Buyer2(10000,0);
+		Tv2 tv = new Tv2();
+		Radio2 radio = new Radio2();
+		Audio2 audio = new Audio2();
+		
+		buyer.buy(tv);
+		buyer.buy(tv);
+		buyer.buy(tv);
+		buyer.buy(tv);
+		buyer.buy(radio);
+		buyer.buy(radio);
+		buyer.buy(audio);
+		buyer.buy(audio);
+		buyer.buy(audio);
+		buyer.buy(audio);
+		buyer.summary();
+	}
+}
---
+++ HappyJava/ProjectList/src/Inherit_Keypoint/Inherit_Keypoint.java
@@ -0,0 +1,85 @@
+package Inherit_Keypoint;
+/*
+<요구사항>
+제품 , 구매자 가 있다
+구매자는 구매행위를 할수있다. 
+잔액 - 제품가격 , 포인트 정보를 갱신
+Key Point -> 구매자는 매장에 있는 모든 물건을 구입할수있다. <- Key Point
+구입가능 물건 3개니까 
+구매할수있는 함수도 3개 여야 한다  
+하나의 이름 , 중복 코드 제거
+모든 제품이 product를 상속(부모)
+아까 배운 자식 참조변수 주소값을 부모 참조타입으로
+*/
+
+class Product {
+	int price;
+	int bonuspoint;
+	
+	Product(int price){
+		this.price = price;
+		this.bonuspoint = (int)(price * 10.0);
+	}
+}
+
+class Tv extends Product {
+	
+	Tv(){
+		super(500);
+	}
+	public String toString() {
+		return "Tv 구입";
+	}
+}
+
+class Computer extends Product {
+	
+	Computer(){
+		super(350);
+	}
+	public String toString() {
+		return "컴퓨터 구입";
+	}
+}
+
+class Radio extends Product {
+	
+	Radio(){
+		super(250);
+	}
+	public String toString() {
+		return "라디오 구입";
+	}
+}
+class Buyer {
+	int money = 2000;
+	int bonuspoint;
+	
+	Buyer(int money , int bonuspoint) {
+		this.money = money;
+		this.bonuspoint = bonuspoint;
+	}
+	
+	public void Buy(Product product) {
+		if(this.money < product.price) {
+			System.out.println("잔액이 부족합니다.");
+		} else  {
+			this.money -= product.price;
+			this.bonuspoint += product.price;
+			System.out.println("구매한 물건 목록은 : " + product.toString());
+			System.out.println("남은 재산 : " + this.money);
+			System.out.println("내 누적 포인트 : " + this.bonuspoint);
+		}
+	}
+}
+public class Inherit_Keypoint {	
+	public static void main(String[] args) {
+		Buyer buyer = new Buyer(2000,0);
+		Tv tv = new Tv();
+		Radio radio = new Radio();
+		Computer computer = new Computer();
+		buyer.Buy(tv);
+		buyer.Buy(radio);
+		buyer.Buy(computer);
+	}
+}
